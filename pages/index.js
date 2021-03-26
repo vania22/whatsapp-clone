3ba26b9ec120543be5edb141c6a014b25575e96e
Ivan Krupskyi <krupskiy111@gmail.com>
2021-03-26 17:08:48 +0000

    Working on Sidebar component. Added Header to the Sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div>
             <Head>
                 <title>WhatsApp 2.0</title>
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <h1>Whatsapp</h1>
+            <Sidebar />
         </div>
     );
 }
