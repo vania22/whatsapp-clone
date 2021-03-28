45e4145ee54c9fc116e1fb13eae7d4801f7cd54c
Ivan Krupskyi <krupskiy111@gmail.com>
2021-03-28 00:58:18 +0000

    Working on a Chat page, fetching chat data from the server

---
+++ components/Chat.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Chat = ({ chat, messages }) => {
+    return <Container>chat</Container>;
+};
+
+export default Chat;
+
+const Container = styled.div``;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,26 +1,74 @@
 import styled from 'styled-components';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import Head from 'next/head';
-import Sidebar from '../../components/Sidebar';
 
-const Chat = () => {
+import { auth, db } from '../../firebase';
+import Sidebar from '../../components/Sidebar';
+import Chat from '../../components/Chat';
+import { getRecipientEmail } from '../../utils/getRecipientEmail';
+
+const ChatPage = ({ chat, messages }) => {
+    const [user] = useAuthState(auth);
+
     return (
         <Container>
             <Head>
-                <title>Chat</title>
+                <title>Chat with {getRecipientEmail(chat.users, user)}</title>
             </Head>
             <Sidebar />
             <ChatContainer>
-                <h1>Hello</h1>
+                <Chat chat={chat} messages={messages} />
             </ChatContainer>
         </Container>
     );
 };
 
-export default Chat;
+export default ChatPage;
+
+export async function getServerSideProps(context) {
+    const ref = db.collection('chats').doc(context.query.id);
+
+    // Prepare messages on the server
+    const messagesRes = await ref.collection('messages').orderBy('timestamp', 'asc').get();
+    const messages = messagesRes.docs
+        .map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }))
+        .map((messages) => ({
+            ...messages,
+            timestamp: messages.timestamp.toDate().getTime(),
+        }));
+
+    // Prepare chats
+    const chatRes = await ref.get();
+    const chat = {
+        id: chatRes.id,
+        ...chatRes.data(),
+    };
+
+    return {
+        props: {
+            messages: JSON.stringify(messages),
+            chat,
+        },
+    };
+}
 
 const Container = styled.div`
     display: flex;
     height: 100vh;
 `;
 
-const ChatContainer = styled.div``;
+const ChatContainer = styled.div`
+    flex: 1;
+    overflow: scroll;
+    height: 100%;
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    --ms-overflow-style: none;
+    scrollbar-width: none;
+`;
